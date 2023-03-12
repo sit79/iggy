c957d602f4b064a2dd8d29a2c81f9f8150df4d60
Stefan Tolksdorf <stefan.tolksdorf@cgi.com>
2023-03-12 14:12:34 +0100

    formatting

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ Node 19.7
 2. Run `npm i`
 3. Create file .env containing:
 
-    `FILEPATH="<path to your tracklists folder>"`
+   `FILEPATH="<path to your tracklists folder>"`
 
-    `SCRAPE_URL="https://www.bbc.co.uk/sounds/brand/b03yblbx"`
+   `SCRAPE_URL="https://www.bbc.co.uk/sounds/brand/b03yblbx"`
 
 4. Create file .config.cjs containing:
 
-    `require("dotenv").config();`
+   `require("dotenv").config();`
 
-    `module.exports = {FILEPATH: process.env.FILEPATH, SCRAPE_URL: process.env.SCRAPE_URL};`
+   `module.exports = {FILEPATH: process.env.FILEPATH, SCRAPE_URL: process.env.SCRAPE_URL};`
--- helper.js
+++ helper.js
@@ -1,29 +1,27 @@
-import ora from 'ora';
+import ora from 'ora'
 
 const createDate = (input) => {
-  const timestamp = input.split(" ").slice(2).reverse().join("-");
-  const releaseDate = new Date(timestamp);
-  let day = releaseDate.getDate().toString();
-  if (day.length === 1) day = '0' + day;
-  let month = (releaseDate.getMonth() + 1).toString();
-  if (month.length === 1) month = '0' + month;
-  const year = releaseDate.getFullYear().toString();
-  return year + '-' + month + '-' + day;
+  const timestamp = input.split(' ').slice(2).reverse().join('-')
+  const releaseDate = new Date(timestamp)
+  let day = releaseDate.getDate().toString()
+  if (day.length === 1) day = '0' + day
+  let month = (releaseDate.getMonth() + 1).toString()
+  if (month.length === 1) month = '0' + month
+  const year = releaseDate.getFullYear().toString()
+  return year + '-' + month + '-' + day
 }
 
 const removeReadMore = (input) => {
-  let array = input.split(" ");
-  if (array[array.length - 1] === "more") {
-    array.pop();
-    if (array[array.length - 1] === "Read") array.pop();
+  let array = input.split(' ')
+  if (array[array.length - 1] === 'more') {
+    array.pop()
+    if (array[array.length - 1] === 'Read') array.pop()
   }
-  return array.join(" ");
-};
+  return array.join(' ')
+}
 
-const _ora = ora(
-  {
-    color: 'white',
-  }
-);
+const _ora = ora({
+  color: 'white',
+})
 
-export { createDate, removeReadMore, _ora };
+export { createDate, removeReadMore, _ora }
